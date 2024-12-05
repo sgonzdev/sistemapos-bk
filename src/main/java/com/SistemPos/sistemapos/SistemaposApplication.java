8192609dc3c3bc305c0836f5d4be0aaf27a2e804
sgonzdev <santiago_gonzalezbe@fet.edu.co>
2024-12-05 15:21:38 -0500

    feat(initial): initial project

---
+++ src/main/java/com/SistemPos/sistemapos/SistemaposApplication.java
@@ -0,0 +1,13 @@
+package com.SistemPos.sistemapos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SistemPos/sistemapos/SistemaposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SistemPos.sistemapos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
